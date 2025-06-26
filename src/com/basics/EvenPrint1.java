8feabdfc295238989b9f7851c46d2e74f585298f
Tushar <tusharbhosale214@gmail.com>
2025-06-26 10:44:07 +0530

    add statement

--- src/com/basics/EvenPrint1.java
+++ src/com/basics/EvenPrint1.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class EvenPrint1 {
 public static void main(String[] args) {
+	System.out.println("print the even number using the stream api java 8");
 	int[] ar= {10,2,45,9,17};
 	List<Integer> list=Arrays.stream(ar).boxed().filter(a -> a%2==0).toList();
 	System.out.println(list);
