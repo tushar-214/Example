bf4118baa618763bac507d369b1568d8a9523489
Tushar <tusharbhosale214@gmail.com>
2025-06-29 08:41:30 +0530

    print even number from array

---
+++ src/com/basics/Array/printEvenFromArray.java
@@ -0,0 +1,18 @@
+package com.basics.Array;
+
+public class printEvenFromArray {
+public static void main(String[] args) {
+	int ar[] = {10,2,34,5,7,89,65};
+	System.out.println("print the even element sort");
+	
+	for(int i=0;i<ar.length;i++)
+	{
+		if(ar[i]%2==0)
+		{
+			System.out.println(ar[i]);
+			
+		}	
+		
+	}
+}
+}
