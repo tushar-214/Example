947ac8ee2bff2b1d54b453fc2fc3c9205478a97a
Tushar <tusharbhosale214@gmail.com>
2025-07-01 09:44:33 +0530

    print charcter ascci

---
+++ src/com/basics/stringdemo/PrintTheCharcter.java
@@ -0,0 +1,13 @@
+package com.basics.stringdemo;
+
+public class PrintTheCharcter {
+	public static void main(String[] args) {
+		
+		// print the ascii code of the given 
+		for(int i=0;i<=100;i++)
+		{
+			System.out.println((char)i);
+		}
+	}
+
+}
