ff49fff3d51500e2f458c4ad8b8e7e204f720fe6
Tushar <tusharbhosale214@gmail.com>
2025-07-04 23:51:51 +0530

    MaxNumber

--- src/com/basics/practice/MaxNumber.java
+++ src/com/basics/practice/MaxNumber.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class MaxNumber {
 public static void main(String[] args) {
 	List<Integer> list=Arrays.asList(10,203,455,345,321,1);
-	int max=list.stream().sorted().max(Integer::compare).orElse(0);
+	int max=list.stream().sorted().max((a,b)-> a-b).orElse(0);
 	System.out.println(max);
 }
 }
