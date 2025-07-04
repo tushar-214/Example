e0cfe5d23bd0abf5bc90b2d1a25f60658f8cb5ca
Tushar <tusharbhosale214@gmail.com>
2025-07-04 23:37:11 +0530

    Eveen Number

---
+++ src/com/basics/practice/EvenNumber.java
@@ -0,0 +1,16 @@
+package com.basics.practice;
+
+import java.util.Arrays;
+
+public class EvenNumber {
+	public static void main(String[] args) {
+		int ar[]= {13,2,3,45,78,54,32,23};
+		
+				Arrays.stream(ar).boxed().distinct().filter( a-> a%2==0).forEach(System.out::print);
+		
+	}
+
+	
+	
+	
+}
