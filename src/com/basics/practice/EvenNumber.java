951314396650181769a51a198db24e526b5c4903
Tushar <tusharbhosale214@gmail.com>
2025-07-05 23:15:45 +0530

    addedd

--- src/com/basics/practice/EvenNumber.java
+++ src/com/basics/practice/EvenNumber.java
@@ -5,12 +5,8 @@ import java.util.Arrays;
 public class EvenNumber {
 	public static void main(String[] args) {
 		int ar[]= {13,2,3,45,78,54,32,23};
-		
-				Arrays.stream(ar).boxed().distinct().filter( a-> a%2==0).forEach(System.out::print);
-		
+				Arrays.stream(ar).boxed().distinct().filter( a-> a%2==0).forEach(System.out::print);	
 	}
 
-	
-	
 	
 }
--- src/com/basics/practice/Min.java
+++ src/com/basics/practice/Min.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 public class Min {
 
+	
 	public static void main(String[] args) {
+		System.out.println("Print min value")
 		List<Integer> list=Arrays.asList(10,203,455,345,321,1);
 		int min=list.stream().sorted().min((a,b)-> a-b).orElse(0);
 		System.out.println(min);
+		
 	}
 }
