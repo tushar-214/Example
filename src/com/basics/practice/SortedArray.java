4ffdc08027cc721ab17eb0d612bea9b7bee21727
Tushar <tusharbhosale214@gmail.com>
2025-07-05 23:13:52 +0530

    added

---
+++ src/com/basics/practice/SortedArray.java
@@ -0,0 +1,15 @@
+package com.basics.practice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SortedArray {
+	public static void main(String[] args) {
+		List<Integer> ar  =Arrays.asList(10,3,4,5,5,2,1,12);
+		
+				
+	}
+
+	
+	
+}
