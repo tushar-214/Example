914327043efd003d1032ba390905ccb638eba9b8
Tushar <tusharbhosale214@gmail.com>
2025-07-06 21:59:14 +0530

    grouping

---
+++ src/practice1/StringGrouping.java
@@ -0,0 +1,15 @@
+package practice1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StringGrouping {
+	public static void main(String[] args) {
+		List<String> list =Arrays.asList("Tushar","ganesh","vishal","blob","slot","gold");
+		Map<Integer, List<String>> grp=list.stream().collect(Collectors.groupingBy(x ->x.length()));
+		System.out.println(grp);
+	}
+
+}
