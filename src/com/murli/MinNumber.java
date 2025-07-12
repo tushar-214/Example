43aba1a763fbae44d8cc8204c59b262a321687ad
Tushar <tusharbhosale214@gmail.com>
2025-07-12 22:40:13 +0530

    min

---
+++ src/com/murli/MinNumber.java
@@ -0,0 +1,14 @@
+package com.murli;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MinNumber {
+	public static void main(String[] args) {
+		List<Integer> list=Arrays.asList(10,20,30,50,10,20);
+
+		Integer minValue=list.stream().min((a,b)->a -b).orElse(0);
+		System.out.println("min value ::"+ minValue);
+	}
+
+}
