e3ba6fe291e5c9ff8f577a7de9cd37d8ada0e176
Tushar <tusharbhosale214@gmail.com>
2025-07-14 18:29:15 +0530

    added

---
+++ src/com/murli/CountLength.java
@@ -0,0 +1,17 @@
+package com.murli;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CountLength {
+	public static void main(String[] args) {
+		
+		List<String> str=Arrays.asList("tushar","atul","tushar","rahul","diksha","rushab","ravindra","atul");
+
+		Map<String, Long> map=str.stream().collect(Collectors.groupingBy(s-> s, Collectors.counting()));
+		System.out.println(map);
+	}
+
+}
