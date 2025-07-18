02cdbe66f2f44283d6e2e03be7f3628e3d75ee3e
TusharBhosale <100034542+tushar-214@users.noreply.github.com>
2025-07-18 21:03:35 +0530

    Update SumOfAddition.java
    Test added

--- src/com/test/SumOfAddition.java
+++ src/com/test/SumOfAddition.java
@@ -2,4 +2,11 @@ package com.test;
 
 public class SumOfAddition {
 System.out.print("hiiii"):
+
+List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+int sum = numbers.stream()
+                 .reduce(0, (a, b) -> a + b);
+System.out.println(sum); // Output: 15
+
+  
 }
