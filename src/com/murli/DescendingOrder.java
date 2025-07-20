da7dbba15bca2a749f7a3d89a001fd7d813a9be5
TusharBhosale <100034542+tushar-214@users.noreply.github.com>
2025-07-20 22:15:44 +0530

    Update DescendingOrder.java
    Add

--- src/com/murli/DescendingOrder.java
+++ src/com/murli/DescendingOrder.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class DescendingOrder {
 	public static void main(String[] args) {
+		// ordersorting and reverse order
 		System.out.println("sort descendiing order ");
 		List<Integer> list=Arrays.asList(10,20,30,50,40,5);
 		list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
