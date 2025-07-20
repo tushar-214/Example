0f76df2bbc16cf031239fcc6cf190d084b84e8c5
TusharBhosale <100034542+tushar-214@users.noreply.github.com>
2025-07-20 22:16:33 +0530

    Update MinNumber.java
    Add

--- src/com/murli/MinNumber.java
+++ src/com/murli/MinNumber.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class MinNumber {
 	public static void main(String[] args) {
+		// min number
 		List<Integer> list=Arrays.asList(10,20,30,50,10,20);
 
 		Integer minValue=list.stream().min((a,b)->a -b).orElse(0);
