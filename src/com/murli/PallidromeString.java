358cf1699a29175f5334f19988f82ebcf832579e
TusharBhosale <100034542+tushar-214@users.noreply.github.com>
2025-07-20 22:17:17 +0530

    Update PallidromeString.java
    Add

--- src/com/murli/PallidromeString.java
+++ src/com/murli/PallidromeString.java
@@ -3,6 +3,7 @@ import java.util.stream.IntStream;
 
 public class PallidromeString {
 	public static void main(String[] args) {
+		// test the pallindroms
 		String str="madam";
 	Boolean res=IntStream.range(0, str.length()-1/2).allMatch(i->str.charAt(i)== str.charAt(str.length()-1-i));
 	System.out.println("given string is pallindrome = " +res);
